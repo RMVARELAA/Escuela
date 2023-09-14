ec64218c04ee20d91b826fb423dcbe90ee317287
ROLANDO MOISES VARELA AMADOR <rmvarela@unah.hn>
2023-09-13 19:52:12 -0600

    Comentarios en el código
    Se han actualizado comentarios en el código

--- Escuela/Controllers/MatriculaController.cs
+++ Escuela/Controllers/MatriculaController.cs
@@ -51,19 +51,19 @@ namespace Escuela.Controllers
         {
             try
             {
-                // Obtén el número de registros para este alumno.
+                // Obtenemos el número de registros para este alumno.
                 int numRegistros = await _context.Matriculas
                     .Where(m => m.IdAlumno == matricula.IdAlumno)
                     .CountAsync();
 
-                // Verifica si el número de registros es menor o igual a 5.
+                // Verificamos si el número de registros es menor o igual a 5.
                 if (numRegistros >= 5)
                 {
                     // Mensaje de error si se supera el límite de 5 clases por alumno.
                     return BadRequest("¡Se ha alcanzado el límite de 5 Clases para este Alumno!.");
                 }
 
-                // Obtener la sección relacionada con la matrícula
+                // Obtenemos la sección relacionada con la matrícula
                 var dbseccion = await _context.Seccions.FindAsync(matricula.IdSeccion);
 
                 if (dbseccion == null)
@@ -71,7 +71,7 @@ namespace Escuela.Controllers
                     return BadRequest("La sección no existe en la base de datos.");
                 }
 
-                // Verificar si la clase ya está matriculada por el mismo alumno
+                // Verificamos si la clase ya está matriculada por el mismo alumno
                 var matriculaExistente = await _context.Matriculas
                     .FirstOrDefaultAsync(m => m.IdAlumno == matricula.IdAlumno && m.IdSeccion == matricula.IdSeccion);
 
@@ -80,7 +80,7 @@ namespace Escuela.Controllers
                     return BadRequest("El alumno ya está matriculado en esta clase.");
                 }
 
-                // Obtener la sección relacionada con la matrícula
+                // Obtenemos la sección relacionada con la matrícula
                 var seccion = await _context.Seccions.FindAsync(matricula.IdSeccion);
 
                 if (seccion == null)
@@ -88,7 +88,7 @@ namespace Escuela.Controllers
                     return BadRequest("La sección no existe en la base de datos.");
                 }
 
-                // Verificar si el alumno tiene una o más veces matriculada la misma clase (aunque sean en diferentes horarios)
+                // Verificamos si el alumno tiene una o más veces matriculada la misma clase (aunque sean en diferentes horarios)
                 var dbclase = await _context.Matriculas
                     .AnyAsync(m => m.IdAlumno == matricula.IdAlumno && m.IdSeccionNavigation != null &&
                     m.IdSeccionNavigation.IdClase != null && m.IdSeccionNavigation.IdClase == seccion.IdClase);
@@ -98,7 +98,7 @@ namespace Escuela.Controllers
                     return BadRequest("El alumno ya está matriculado en una sección de esta clase.");
                 }
 
-                // Verificar si el alumno ya está matriculado en una sección con la misma hora inicial
+                // Verificamos si el alumno ya está matriculado en una sección con la misma hora inicial
                 var seccionesConMismaHora = await _context.Matriculas
                     .Include(m => m.IdSeccionNavigation)
                     .Where(m => m.IdAlumno == matricula.IdAlumno && m.IdSeccionNavigation != null &&
@@ -132,19 +132,19 @@ namespace Escuela.Controllers
                 if (dbmatricula == null)
                     return BadRequest("Clase matriculada no encontrada");
 
-                // Obtén el número de registros para este alumno.
+                // Obtenemos el número de registros para este alumno.
                 int numRegistros = await _context.Matriculas
                     .Where(m => m.IdAlumno == request.IdAlumno)
                     .CountAsync();
 
-                // Verifica si el número de registros es menor o igual a 5.
+                // Verificamos si el número de registros es menor o igual a 5.
                 if (numRegistros >= 5)
                 {
                     // Mensaje de error si se supera el límite de 5 clases por alumno.
                     return BadRequest("¡Se ha alcanzado el límite de 5 Clases para este Alumno!.");
                 }
 
-                // Obtener la sección relacionada con la matrícula
+                // Obtenemos la sección relacionada con la matrícula
                 var dbseccion = await _context.Seccions.FindAsync(request.IdSeccion);
 
                 if (dbseccion == null)
@@ -152,7 +152,7 @@ namespace Escuela.Controllers
                     return BadRequest("La sección no existe en la base de datos.");
                 }
 
-                // Verificar si la clase ya está matriculada por el mismo alumno
+                // Verificamos si la clase ya está matriculada por el mismo alumno
                 var matriculaExistente = await _context.Matriculas
                     .FirstOrDefaultAsync(m => m.IdAlumno == request.IdAlumno && m.IdSeccion == request.IdSeccion);
 
@@ -161,7 +161,7 @@ namespace Escuela.Controllers
                     return BadRequest("El alumno ya está matriculado en esta clase.");
                 }
 
-                // Obtener la sección relacionada con la matrícula
+                // Obtenemos la sección relacionada con la matrícula
                 var seccion = await _context.Seccions.FindAsync(request.IdSeccion);
 
                 if (seccion == null)
@@ -169,7 +169,7 @@ namespace Escuela.Controllers
                     return BadRequest("La sección no existe en la base de datos.");
                 }
 
-                // Verificar si el alumno tiene una o más veces matriculada la misma clase (aunque sean en diferentes horarios)
+                // Verificamos si el alumno tiene una o más veces matriculada la misma clase (aunque sean en diferentes horarios)
                 var dbclase = await _context.Matriculas
                     .AnyAsync(m => m.IdAlumno == request.IdAlumno && m.IdSeccionNavigation != null &&
                     m.IdSeccionNavigation.IdClase != null && m.IdSeccionNavigation.IdClase == seccion.IdClase);
@@ -179,7 +179,7 @@ namespace Escuela.Controllers
                     return BadRequest("El alumno ya está matriculado en una sección de esta clase.");
                 }
 
-                // Verificar si el alumno ya está matriculado en una sección con la misma hora inicial
+                // Verificamos si el alumno ya está matriculado en una sección con la misma hora inicial
                 var seccionesConMismaHora = await _context.Matriculas
                     .Include(m => m.IdSeccionNavigation)
                     .Where(m => m.IdAlumno == request.IdAlumno && m.IdSeccionNavigation != null &&
